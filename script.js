75ede240a950cb29f0049bbdac87d73d9525951b
Abhinash01 <abinashgupa111@gmail.com>
2025-04-16 23:05:48 +0530

    some changes here also

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const scroll = new LocomotiveScroll({
 
 document.addEventListener("DOMContentLoaded", function () {
     const menuIcon = document.querySelector(".ri-menu-3-line");
-    const navLinks = document.querySelectorAll("nav a");
+    const navLinks = document.querySelectorAll("nav a, .mobile-nav a");
   
     menuIcon.addEventListener("click", function () {
       document.body.classList.toggle("nav-open");
