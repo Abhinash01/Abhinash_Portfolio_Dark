1c454e3e099389899478e5780258b2dbb132022a
Abhinash01 <abinashgupa111@gmail.com>
2025-04-16 08:15:20 +0530

    Trying JS

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.querySelector(".ri-menu-3-line");
+    const navLinks = document.querySelectorAll("nav a");
+  
+    menuIcon.addEventListener("click", function () {
+      document.body.classList.toggle("nav-open");
+    });
+  
+    navLinks.forEach(link => {
+      link.addEventListener("click", () => {
+        document.body.classList.remove("nav-open");
+      });
+    });
+  });
+  
